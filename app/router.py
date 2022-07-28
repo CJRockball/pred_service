12ff4ef5a5e52cb3ccce4981daf3f3c6b8d4f9b0
Patrick Carlberg <patrickcj@carlberg.it>
2022-07-28 13:53:27 +0800

    v0

---
+++ app/router.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.main import predict
+from fastapi.staticfiles import StaticFiles
+import pathlib
+
+ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
+TEMP_PATH = ROOT_DIR / "templates"
+STAT_PATH = ROOT_DIR / "static"
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=STAT_PATH), name="static")
+app.include_router(predict, prefix='/predict', tags=['predict'])
